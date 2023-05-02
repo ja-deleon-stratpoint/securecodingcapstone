6b359543dcefe6acd87775d397b7b3eb16c35be7
ja-deleon-stratpoint <132338976+ja-deleon-stratpoint@users.noreply.github.com>
2023-05-02 16:45:11 +0800

    Update research.js

--- app/routes/research.js
+++ app/routes/research.js
@@ -12,7 +12,7 @@ function ResearchHandler(db) {
     this.displayResearch = (req, res) => {
 
         if (req.query.symbol) {
-            const url = req.query.url + req.query.symbol;
+            const url = 'https://finance.yahoo.com/quote/' + req.query.symbol;
             return needle.get(url, (error, newResponse, body) => {
                 if (!error && newResponse.statusCode === 200) {
                     res.writeHead(200, {
